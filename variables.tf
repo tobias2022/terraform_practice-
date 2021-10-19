4cba3f8c4f036431744d8d7d1084b0d86c5afde9
tobias2022 <89058974+tobias2022@users.noreply.github.com>
2021-10-19 19:44:41 +1100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "endpoint" {
+  description = "Endpoint url"
+  type        = string
+  default    = "tobais.tk"
+}
+
+variable "domain_name" {
+  description = "Domain name"
+  type        = string
+  default     = "tobias.tk"
+}
